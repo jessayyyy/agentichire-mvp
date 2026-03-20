1068d21b9ea55614a6754d44e90bbcb487a422cf
jessayyyy <jessiechutingern@gmail.com>
2026-03-20 14:05:47 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import EmployerDashboard from './pages/EmployerDashboard'
-import CreateAssessment from './pages/CreateAssessment'
-import TakeAssessment from './pages/TakeAssessment'
-import ViewResults from './pages/ViewResults'
+import EmployerDashboard from './pages/EmployerDashboard.jsx'
+import CreateAssessment from './pages/CreateAssessment.jsx'
+import TakeAssessment from './pages/TakeAssessment.jsx'
+import ViewResults from './pages/ViewResults.jsx'
 
 function App() {
   return (
