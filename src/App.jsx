e9ef36d12b3cc5eb1bbc8eac90724ceb592b2f16
jessayyyy <jessiechutingern@gmail.com>
2026-03-20 13:35:52 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import EmployerDashboard from './pages/EmployerDashboard'
+import CreateAssessment from './pages/CreateAssessment'
+import TakeAssessment from './pages/TakeAssessment'
+import ViewResults from './pages/ViewResults'
 
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gradient-to-b from-blue-100 to-blue-600">
+        <Routes>
+          <Route path="/" element={<EmployerDashboard />} />
+          <Route path="/create" element={<CreateAssessment />} />
+          <Route path="/assessment/:linkId" element={<TakeAssessment />} />
+          <Route path="/results/:assessmentId" element={<ViewResults />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
